3aab606a3b641fb0c6c129844f306fb9238f03d2
John Anderson <sontek@gmail.com>
2015-05-26 15:20:24 -0700

    Added shell

---
+++ pyramid_cli/cli/shell.py
@@ -0,0 +1,31 @@
+import click
+from pyramid_cli.cli import main
+from montague import load_app, load_server
+from IPython.terminal.embed import InteractiveShellEmbed
+from pyramid.scripting import prepare
+
+@main.command()
+@click.option(
+    '--shell', default='ipython', type=click.Choice(['ipython', 'bpython'])
+)
+@click.pass_obj
+def shell(config, shell):
+    app = load_app(config)
+    env = prepare()
+    env['app'] = app
+
+    values = {
+        'app': 'The WSGI application',
+        'registry': 'Active Pyramid registry',
+        'request': 'Active request object',
+    }
+    banner = 'Environment:'
+
+    for key in sorted(values.keys()):
+        banner += '\n %-12s %s' % (key, values[key])
+
+    if shell == 'ipython':
+        InteractiveShellEmbed(banner2=banner, user_ns=env)()
+    elif shell == 'bpython':
+        from bpython import embed
+        embed(locals_=env, banner=banner)()
